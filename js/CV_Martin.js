d0acb22e3fc3f180b89c50eead8e1396049d241f
Martin Encinas <encinasmartin@gmail.com>
2023-01-29 23:56:47 +0100

    primer escritura en el repositorio trabajo final

---
+++ js/CV_Martin.js
@@ -0,0 +1,3 @@
+document.getElementById("boton").addEventListener('click', function () {
+    document.getElementById("demo").innerHTML = "e-mail: encinasmar@gmail.com  Teléfono: +54 9 351 1234567"
+});
